89d59aa1696a248502d95ce8c4836ec88483af71
sakya_sekhar <sakyasekhar.g@gmail.com>
2023-04-04 15:17:42 +0530

    changing port from 3000 to

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const io = new Server(server)
 const dbURI = "mongodb+srv://Sakyasekhar:sakyasekhar@chatapp.pjjecjh.mongodb.net/?retryWrites=true&w=majority"; 
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => server.listen(3000))
+  .then(result => server.listen(process.env.PORT || 3030))
   .catch(err => console.log(err));
 
 app.set('view engine','ejs');
